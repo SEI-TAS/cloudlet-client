ebb10cb2691ac416e333b53b1b599852a0153437
secheverria <sebastian.echeverria@gmail.com>
2015-05-22 12:28:17 -0300

    SKA: implemented file receiving through Bluetooth. Only shows the file contents in the log, it does not save the file. Not yet tested.

--- src/main/java/edu/cmu/sei/cloudlet/client/ska/MessageHandler.java
+++ src/main/java/edu/cmu/sei/cloudlet/client/ska/MessageHandler.java
@@ -10,12 +10,22 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
+ * Handles message receiving, sending, and processing.
  * Created by Sebastian on 2015-05-20.
  */
 public class MessageHandler {
     private final String TAG = "MessageHandler";
 
     private final String CMD_GET_ID = "id";
+    private final String CMD_FILE_MASTER_PUBLIC_KEY = "master_public_key";
+    private final String CMD_FILE_DEVICE_PRIVATE_KEY = "device_private_key";
+    private final String CMD_FILE_SERVER_CERTIFICATE = "server_certificate";
+    private final String CMD_FILE_END = "file_end";
+
+    private final String REPLY_ACK = "ack";
+
+    private final int CHUNK_SIZE = 4096;
+    private final int MAX_FILE_SIZE = 10*1024*1024;
 
     private final BluetoothSocket mSocket;
     private InputStream mInStream = null;
@@ -35,10 +45,13 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * Receives messages until disconnected.
+     */
     public void receiveMessages() {
         Log.v(TAG, "Receiving messages");
         int num_bytes;
-        byte[] buffer = new byte[4096];
+        byte[] buffer = new byte[CHUNK_SIZE];
         String message = null;
 
         while (true) {
@@ -61,17 +74,53 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * Handles a received message.
+     * @param message a command from a cloudlet.
+     */
     private void handleMessage(String message) {
         if(message.equals(CMD_GET_ID)) {
-            Log.v("Test", "id request");
+            Log.v(TAG, "id request");
             String androidId = Settings.Secure.getString(
                     mContext.getContentResolver(), Settings.Secure.ANDROID_ID);
-            Log.v("Test", androidId);
+            Log.v(TAG, androidId);
 
+            // Send the reply.
             sendMessage(androidId);
+        }
+        else if (message.equals(CMD_FILE_MASTER_PUBLIC_KEY)) {
+            Log.v(TAG, "master public key file request");
+            sendMessage(REPLY_ACK);
+
+            byte[] fileData = receiveFile();
+            String fileAsString = new String(fileData);
+            Log.v(TAG, "Data in file:");
+            Log.v(TAG, fileAsString);
+        }
+        else if (message.equals(CMD_FILE_DEVICE_PRIVATE_KEY)) {
+            Log.v(TAG, "master public key file request");
+            sendMessage(REPLY_ACK);
+
+            byte[] fileData = receiveFile();
+            String fileAsString = new String(fileData);
+            Log.v(TAG, "Data in file:");
+            Log.v(TAG, fileAsString);
+        }
+        else if (message.equals(CMD_FILE_SERVER_CERTIFICATE)) {
+            Log.v(TAG, "master public key file request");
+            sendMessage(REPLY_ACK);
+
+            byte[] fileData = receiveFile();
+            String fileAsString = new String(fileData);
+            Log.v(TAG, "Data in file:");
+            Log.v(TAG, fileAsString);
         }
     }
 
+    /**
+     * Sends a message to the cloudlet.
+     * @param message
+     */
     public void sendMessage(String message) {
         byte[] send = message.getBytes();
         try {
@@ -79,5 +128,50 @@ public class MessageHandler {
         } catch (IOException e) {
             Log.e(TAG, "Problem sending message.", e);
         }
+    }
+
+    /**
+     * Receives a file and returns it as a byte array.
+     * @return A byte array with the file data.
+     */
+    protected byte[] receiveFile() {
+        Log.v(TAG, "Receiving file");
+        int numBytes;
+        byte[] fileData = new byte[MAX_FILE_SIZE];
+        int fileDataSize = 0;
+        byte[] buffer = new byte[CHUNK_SIZE];
+
+        try {
+            while(true) {
+                numBytes = mInStream.read(buffer);
+                Log.v(TAG, "Num bytes: " + numBytes);
+
+                if (numBytes > 0) {
+                    byte[] chunkBytes = new byte[numBytes];
+                    System.arraycopy(buffer, 0, chunkBytes, 0, numBytes);
+
+                    // End of file command will only arrive by itself, in a smaller size than CHUNK_SIZE.
+                    if (numBytes < CHUNK_SIZE) {
+                        String message = new String(chunkBytes);
+                        if (message.equals(CMD_FILE_END)) {
+                            Log.v(TAG, "End of file received");
+                            break;
+                        }
+                    }
+                    else {
+                        // Accumulate data into the overall file data buffer.
+                        System.arraycopy(buffer, 0, fileData, fileDataSize, numBytes);
+                        fileDataSize += numBytes;
+                    }
+                }
+            }
+        } catch (IOException e) {
+                Log.e(TAG, "No longer connected.");
+        }
+
+        // Create a buffer with the exact size of the received data.
+        byte[] realFileData = new byte[fileDataSize];
+        System.arraycopy(fileData, 0, realFileData, 0, fileDataSize);
+        return realFileData;
     }
 }
